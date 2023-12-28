25ab118cfbc7282286e6dd2d7a1fa862f76a6655
Vidhya S <147298749+Vidhya-Soloman@users.noreply.github.com>
2023-12-29 00:14:25 +0530

    Add files via upload

--- login/loginpage.php
+++ login/loginpage.php
@@ -11,7 +11,7 @@
 <tr><th>Email id:</th>
 <td><input type="textbox" name="email"></input></td></tr>
 <tr><th>Password</th>
-<td><input type="password" name="pass"></input></td></tr>
+<td><input type="password" name="password"></input></td></tr>
 <tr>
 <td colspan="2" align="center"><input type="submit" name="submit" value="submit" style=background-color:lightgreen>
 <input type="reset" name="reset" style=background-color:skyblue>
@@ -19,7 +19,24 @@
 <div align="center">
 <br><br>
 Not a member? <a href="frameset.php" >Login here</a></div>
-
 </form>
 </body>
 </html>
+
+<?php
+$con=mysqli_connect('localhost','root','','login');
+if($con)
+//echo "connected to database";
+if(isset($_POST['submit']))
+{
+$name=$_POST['name'];
+$email=$_POST['email'];
+$password=$_POST['password'];
+$q="select * from student where name='$name' and email='$email' and password='$password'";
+$cq=mysqli_query($con,$q);
+if(mysqli_num_rows($cq))
+{
+Header("Location:frameset.php");
+}
+}
+?>
